1a975c8f594909695bbe97480a1b640c39322c01
Your Name <vica031106@gmail.com>
2024-02-12 20:58:18 -0800

    aa

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,14 @@
 // place files you want to import through the `$lib` alias in this folder.
+let tempData = [];
+
+onMount(async () => {
+
+    const res = await fetch('energy.csv'); 
+
+    const csv = await res.text();
+
+    tempData = d3.csvParse(csv, d3.autoType)
+
+    console.log(tempData);
+
+});
