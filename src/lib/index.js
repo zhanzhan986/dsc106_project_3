542761db80cc3917a0112ace1dc6ed804227b3dd
zhanzhan986 <117331602+zhanzhan986@users.noreply.github.com>
2024-02-12 21:59:13 -0800

    load data

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,14 @@
 // place files you want to import through the `$lib` alias in this folder.
+let energyData = [];
+
+onMount(async () => {
+
+    const res = await fetch('energy.csv'); 
+
+    const csv = await res.text();
+
+    energyData = d3.csvParse(csv, d3.autoType)
+
+    console.log(energyData);
+
+});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+    import { onMount } from 'svelte';
+    import * as d3 from 'd3';
+    let tempData = [];
+  
+    onMount(async () => {
+      const res = await fetch('energy.csv');
+      const csv = await res.text();
+      tempData = d3.csvParse(csv, d3.autoType);
+      console.log(tempData);
+    });
+  </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
