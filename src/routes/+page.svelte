505a6430bedb44bdf982468a2c9057751e338f89
zhanzhan986 <117331602+zhanzhan986@users.noreply.github.com>
2024-02-12 22:05:43 -0800

    change dataset name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script>
     import { onMount } from 'svelte';
     import * as d3 from 'd3';
-    let tempData = [];
+    let energyData = [];
   
     onMount(async () => {
       const res = await fetch('energy.csv');
       const csv = await res.text();
-      tempData = d3.csvParse(csv, d3.autoType);
-      console.log(tempData);
+      energyData = d3.csvParse(csv, d3.autoType);
+      console.log(energyData);
     });
   </script>
 
